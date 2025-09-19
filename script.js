7b5f610adfeed5e459c17bf2efc2e413dc51dbf2
thyking3 <victoryung006@gmail.com>
2025-09-19 11:43:01 +0100

    worked on the refsactoting

--- script.js
+++ script.js
@@ -1,54 +1,67 @@
-// Get Element
-const todoInput = document.getElementById('todo-input');
-const addTodoBtn = document.getElementById('add-todo-btn');
-const todoList = document.getElementById('todo-list');
+//getElement
 
-// initialize todos arrayn
-let todos = [];
+const todoInput = document.getElementById("todo-input")
+const addTodoBtn = document.getElementById("add-todo-btn")
+const todoList = document.getElementById("todo-list")
 
-// add event listener to addButton
+//initialize todo array
+
+let todos = []
+
+// add event listner to addButton
 addTodoBtn.addEventListener('click', addTodo);
 
-// fuction to add todo
+//fumction add todo
 
-function addTodo() {
-    const todoText = todoInput.value.trim();
-    if(todoText){
-        const newTodo ={
-            text: todoText,
-            completed:true
-        };
-        todos.push(newTodo);
-        renderTodos();
-        todoInput.value = '';
-    }
+function addTodo(){
+  const todoText = todoInput.value.trim()
+  if(todoText){
+    const newTodo = {
+      text: todoText,
+      completed:true
+    };
+    todos.push(newTodo)
+    renderTodos();
+    todoInput.value ='';
+  }
+
+
 }
 
-// function to render todos
+// funtion to render tools
 function renderTodos(){
-    todoList.innerHTML = '';
-    todos.forEach((todo, index) =>{
-        const todoItem = document.createElement('li');
-        todoItem.classList.add('todo-item')
-        if(todo.completed){
-            todoItem.classList.add('completed')
-        }
-        todoItem.innerHTML= `
-        <span>${todo.text}</span>
-        <button class='complete-btn'>Complete</button>
-        <button class='delete-btn'>Delete</button>
-        `;
-        todoList.appendChild(todoItem);
+  todoList.innerHTML = '';
+  todos.forEach((todo, index) =>{
+    const todoItem = document.createElement("li");
+    todoItem.classList.add('todo-item')
+    if( todo.completed){
+      todoItem.classList.add("completed")
+    }
+    todoItem.innerHTML= `
+    <span> ${todo.text}</span>
+    <button class='complete-btn'> Completed </button>
+    <button class='delete-btn'> Delete </button>
+    
+    `;
+    todoList.appendChild(todoItem);
 
-        // add event listeners to buttons
+    // add event listner to buttons
+    todoItem.querySelector('.complete-btn').addEventListener
 
-        todoItem.querySelector('.complete-btn').addEventListener('click', () =>{
-            todo.completed = !todo.completed;
-            renderTodos()
-        });
-        todoItem.querySelector('.delete-btn').addEventListener('click', () =>{
-            todos.splice(index, 1);
-            renderTodos()
-        });
-    });
-}
+    ('click', () => {
+      todo.completed = !todo.completed;
+      renderTodos()
+    })
+
+    todoItem.querySelector('.delete-btn').addEventListener
+  ('click', () =>{
+    todos.splice(index, 1);
+    renderTodos()
+  })
+
+
+  }
+
+)
+} 
+ 
